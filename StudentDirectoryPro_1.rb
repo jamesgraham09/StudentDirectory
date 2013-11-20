e7c71123e7f385196cde1f3ba88051fe6586eeae
JamesGraham <jamesgraham09@gmail.com>
2013-11-20 16:20:25 +0000

    DirectoryPro 1.0

--- StudentDirectoryPro_1.rb
+++ StudentDirectoryPro_1.rb
@@ -1,8 +1,37 @@
 # create a template for user to input names
+# Print the menu and ask user what to do
+# Read input and save it
+# Repeat
 
+# METHOD TO work out what user wants to do
+def interactive_menu
+# print introductory question
+	students =[]
+	loop do
+		puts "What would you like to do now?"
+		puts "1. input names"
+		puts "2. review student list"
+		puts "9. exit the program"	
+# get selection
+		selection = gets.chomp		
+# establish consequences of selection	
+		case selection
+			when "1"
+				students = input_students(students)
+			when "2"
+				print_students(students)
+			when "9"
+				exit
+			else
+				puts "Not understood. Re-input selection"
+		end
+	end
+end
+
+# METHODS FOR HEADER AND FOOTER TEXT
 def print_header
-	puts "Student Directory Project, James Graham, Nov 13"
 	puts "-----------------------------------------------"
+	puts "Student Directory Project, James Graham, Nov 13"
 	puts ""
 end
 
@@ -11,10 +40,10 @@ def print_footer
 	puts "-----------------------------------------------"
 end
 
-def input_students
+# METHOD TO enter users into the system
+def input_students(students)
 	puts "Please enter student names"
 	puts "Hit return twice to exit"
-	students =[]
 	name = gets.chomp
 	while !name.empty? do
 		students << {:name => name, :cohort => "november"}
@@ -25,15 +54,13 @@ def input_students
 end
 
 def  print_students(y)
-	
+	print_header
 	y.each do |z|
 		print z[:name]
 		print ", "
 		puts z[:cohort]
 	end
+	print_footer
 end
 
-x = input_students
-print_header
-print_students(x)
-print_footer
+interactive_menu
